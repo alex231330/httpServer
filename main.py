e57f8049425d9ea0e822c1e4b4903025c5a65481
alex231330 <alexklatch@gmail.com>
2018-06-06 23:54:15 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import ssl
 
+from io import BytesIO
+
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
@@ -8,8 +10,21 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b'Hello, world!')
 
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
 
-httpd = HTTPServer(('localhost', 4443), SimpleHTTPRequestHandler)
+
+
+
+httpd = HTTPServer(('159.65.133.152', 4443), SimpleHTTPRequestHandler)
 
 httpd.socket = ssl.wrap_socket (httpd.socket, 
         keyfile="key.pem", 
