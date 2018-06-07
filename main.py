c2bc7f6418bcedcb524dc157e2663a8afdff146d
alex231330 <alexklatch@gmail.com>
2018-06-07 10:32:29 +0300

    Removed SSL

--- main.py
+++ main.py
@@ -88,37 +88,8 @@ class Server:
 
          if (request_method == 'GET') | (request_method == 'HEAD'):
 
-             file_requested = string.split(' ')
-             file_requested = file_requested[1] 
-
-             file_requested = file_requested.split('?')[0] 
-
-             if (file_requested == '/'): 
-                 file_requested = '/index.html' 
-
-             file_requested = self.www_dir + file_requested
-             print ("Serving web page [",file_requested,"]")
-
-             try:
-                 file_handler = open(file_requested,'rb')
-                 if (request_method == 'GET'): 
-                     response_content = file_handler.read() 
-                 file_handler.close()
-
-                 response_headers = self._gen_headers( 200)
-
-             except Exception as e:
-                 print ("Warning, file not found. Serving response code 404\n", e)
-                 response_headers = self._gen_headers( 404)
-
-                 if (request_method == 'GET'):
-                    response_content = b"<html><body><p>Error 404: File not found</p><p>Python HTTP server</p></body></html>"
-
-             server_response =  response_headers.encode()
-             if (request_method == 'GET'):
-                 server_response +=  response_content  
-
-             conn.send(server_response)
+             print (request_method)
+             conn.send(b'server_response')
              print ("Closing connection with client")
              conn.close()
 
