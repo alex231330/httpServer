8865f850f48f6a521f191e97ce2c92113dd81748
alex231330 <alexklatch@gmail.com>
2018-06-07 23:20:39 +0300

    Add POST requests handler

--- main.py
+++ main.py
@@ -86,6 +86,7 @@ class Server:
 
                 print (request_method)
                 print(data)
+                current_date = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
                 req = 'HTTP/1.1 200 OK\n Date: ' + current_date + '\n Server: Simple-Python-HTTP-Server\n Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT\n Content-Length: 88\n Content-Type: text/html \n Connection: close\n\n <html><body><h1>Hello, World!</h1></body></html>'
                 conn.close()
             else:
